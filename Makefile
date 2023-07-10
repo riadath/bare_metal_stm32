62f06734bd0c1f730eeaa15203be56fee084febd
riadath <akib152012@gmail.com>
2023-07-11 00:18:26 +0600

    blinky bare metal final version

--- Makefile
+++ Makefile
@@ -3,7 +3,9 @@ CPU = cortex-m4
 CFLAGS = -c -mcpu=$(CPU) -mthumb -std=gnu11 -Wall -O0
 LDFLAGS = -nostdlib -T linker.ld -Wl,-Map=out.map
 
+
 all:main.o stm32_startup.o out.elf
+clean_build: clean all	
 main.o:main.c
 	$(CC) $(CFLAGS) $^ -o $@
 stm32_startup.o:stm32_startup.c
@@ -12,5 +14,7 @@ out.elf: main.o stm32_startup.o
 	$(CC) $(LDFLAGS) $^ -o $@
 
 clean:
-	del *.o, *.elf
+	del *.o, *.elf, *.map
 
+load:
+	openocd -f board/st_nucleo_f4.cfg
--- temp_save.c
+++ temp_save.c
@@ -11,8 +11,6 @@
   n = 7, for GPIOH
 */
 #define GPIO(n) ((struct GPIO_STRUCT *) (0x40020000 + (n * 0x400)))
-#define GPIOA_OFFSET 0x0
-#define AHB1_OFFSET 0x30
 
 
 struct GPIO_STRUCT {
@@ -28,15 +26,14 @@ struct GPIO_STRUCT {
 };
 
 
-
 static inline void GPIO_Init(uint8_t gpio_no, uint8_t pin,uint8_t mode)
 {
   struct GPIO_STRUCT* gpio = GPIO(gpio_no);
-  gpio->MODER &= ~(3U << (pin * 2));
-  gpio->MODER |= ((mode) << (pin * 2));
+  gpio->MODER &= ~(0x3 << (pin * 2));
+  gpio->MODER |= ((mode & 3) << (pin * 2));
 }
 
-static inline void GPIO_WritePin(uint8_t gpio_no,uint8_t pin,bool state){
+static inline GPIO_WritePin(uint8_t gpio_no,uint8_t pin,bool state){
   struct GPIO_STRUCT *gpio = GPIO(gpio_no);
 
   if(state == true){
@@ -46,32 +43,19 @@ static inline void GPIO_WritePin(uint8_t gpio_no,uint8_t pin,bool state){
   }
 }
 
-static inline void shitty_delay(volatile uint32_t count) {
-  while (count--) asm("nop");
+void shitty_delay(){
+  for (int i = 0;i < 1e9;i++);
 }
-
 
 int main()
 {
-  volatile uint32_t *RCC_AHB1ENR = (uint32_t*)(0x40023800 + AHB1_OFFSET);
-  *RCC_AHB1ENR |= (1 << GPIOA_OFFSET); //enable GPIOA
-
   GPIO_Init(0,5,1);
-  GPIO_Init(0,6,1);
-  GPIO_Init(0,7,1);
 
   while(1){
     GPIO_WritePin(0,5,true);
-    GPIO_WritePin(0,7,true);
-    GPIO_WritePin(0,6,true);
-
-    shitty_delay(999999);
-
+    shitty_delay();
     GPIO_WritePin(0,5,false);
-    GPIO_WritePin(0,7,false);
-    GPIO_WritePin(0,6,false);
-
-    shitty_delay(999999);
+    shitty_delay();
   }
   
   return 0;
